eca3fb7778c93399d9769134cb52cbfbfb7f6f10
AdrianoAlmiron <almironadriano89@gmail.com>
2022-04-26 23:41:52 -0300

    NavBar

---
+++ src/componets/navegacion/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import '../style/NavBar.css';
+import logo from '../imagenes/logo.png';
+
+const NavBar = () => {
+    return (
+      <header className="header">
+      <div className="logo-container">
+        <div >
+        <img className="logo-img" src={logo} alt="logo"/>
+        </div> 
+      </div>
+      <div>
+      <input type="text" placeholder="buscar" required></input>
+      </div>
+
+      <nav>
+        <ul className="nav-container">
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/">Crear</a>
+          </li>
+          <li>
+            <a href="/">Notificaciones</a>
+          </li>
+          <li>
+            <a href="/">Perfil</a>
+          </li>
+        </ul>
+      </nav>
+
+      <select className="select-language">
+        <option value="" selected disabled>
+          Language
+        </option>
+        <option value="">Español</option>
+        <option value="">日本語</option>
+      </select>
+      </header>
+    );
+};
+
+export default NavBar;
